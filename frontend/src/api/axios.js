a208254edfe5bf894bba9fd36fc912c95a6982ab
Laukik Lanje <laukiklanje24@gmail.com>
2026-03-13 12:46:41 +0530

    update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -14,4 +14,19 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      // token expired
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      window.location.href = "/";
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default API;
